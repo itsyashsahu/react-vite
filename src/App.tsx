d86acb8ea320957884e13a7346fd2b01ae417dff
itsyashsahum <itsyashsahu@gmail.com>
2024-05-02 22:45:37 +0530

    Fallback component added to handle the case of the suspending in the

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import routes from './routes'
-import { Helmet } from 'react-helmet';
-import Config from './config';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import routes from "./routes";
+import { Helmet } from "react-helmet";
+import Config from "./config";
+import { Suspense } from "react";
+import { LoadingCardFallback } from "./components/LoadingCard";
 
-
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 const router = createBrowserRouter(routes);
 
 function App() {
   return (
     <>
-    <QueryClientProvider client={queryClient}>
-      <Helmet>
+      <QueryClientProvider client={queryClient}>
+        <Helmet>
           <meta charSet="utf-8" />
           <title>Cartoonify</title>
           <link rel="canonical" href={`${Config.FRONTEND_URL}/`} />
           <link rel="icon" type="image/png" href="logo.ico" sizes="16x16" />
-      </Helmet>
-      <div className="flex flex-col w-full min-h-screen bg-background bg-pattern">
-        <RouterProvider router={router} />
-      </div>
-    </QueryClientProvider>
+        </Helmet>
+        <Suspense fallback={<LoadingCardFallback />}>
+          <div className="flex flex-col w-full min-h-screen bg-background bg-pattern">
+            <RouterProvider router={router} />
+          </div>
+        </Suspense>
+      </QueryClientProvider>
     </>
-  )
+  );
 }
 
 export { queryClient };
-export default App
+export default App;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,39 +1,40 @@
-import { RouteObject } from "react-router-dom"
-import { Suspense, lazy } from 'react';
+import { RouteObject } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import LoadingCard, { LoadingCardFallback } from "./components/LoadingCard";
 
-const Dashboard = lazy(() => import('./pages/Dashboard'));
-const Gallery = lazy(() => import('./pages/Gallery'));
-const Character = lazy(() => import('./pages/Character'));
-const Search = lazy(() => import('./pages/Search'));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Gallery = lazy(() => import("./pages/Gallery"));
+const Character = lazy(() => import("./pages/Character"));
+const Search = lazy(() => import("./pages/Search"));
 
 const routes: RouteObject[] = [
-    {
-        path: "/",
-        element: (<Dashboard />),
-    },
-    {
-        path: "/gallery",
-        element: (
-            <Suspense fallback={<div>Loading...</div>}>
-                <Gallery />
-            </Suspense>
-        ),
-    },
-    {
-        path: "/character/:id",
-        element: (
-            <Suspense fallback={<div>Loading...</div>}>
-                <Character />
-            </Suspense>
-        ),
-    },
-    {
-        path: "/search",
-        element: (
-            <Suspense fallback={<div>Loading...</div>}>
-                <Search />
-            </Suspense>
-        ),
-    }
-]
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/gallery",
+    element: (
+      <Suspense fallback={<LoadingCardFallback />}>
+        <Gallery />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/character/:id",
+    element: (
+      <Suspense fallback={<LoadingCardFallback />}>
+        <Character />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/search",
+    element: (
+      <Suspense fallback={<LoadingCardFallback />}>
+        <Search />
+      </Suspense>
+    ),
+  },
+];
 export default routes;
